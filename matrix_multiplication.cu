5efc526ff1ecc87191945183235b906475599faf
daju233 <2212509690@qq.com>
2025-01-02 20:33:45 +0800

    基本实现

--- matrix_multiplication.cu
+++ matrix_multiplication.cu
@@ -1,32 +1,51 @@
 #include <iostream>
 #include <vector>
-#include <iomanip>
 #include <cmath>
 
 __global__ void MatrixMulKernel(float *M, float *N, float *P, int width)
 {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
-    if ((row < width) && (col < width))
+    if ((row < width) && (col < width))//row col = 0,1
     {
         float Pvalue = 0;
-        for (int k = 0; k < width; ++k)
+        for (int k = 0; k < width; ++k)//k<=2
         {
             Pvalue += M[row * width + k] * N[k * width + col];
-        }
+        }        
         P[row * width + col] = Pvalue;
     }
 }
 
 int main()
 {
-    const int width = 4;
-    size_t size = width * width * sizeof(float);
-    std::vector<float> h_M(width * width);
-    std::vector<float> h_N(width * width);
-    std::vector<float> h_P(width * width, 0.0f);
-    std::vector<float> h_ref(width * width, 0.0f);
+    const int width = 2;
+    float *M_d, *N_d, *P_d;
 
-    float *d_M, *d_N, *d_P;
-    cudaMalloc()
+    size_t size = width * width * sizeof(float);
+
+    std::vector<float> h_Matrix_one(width * width, 4.0f);
+    std::vector<float> h_Matrix_two(width * width, 2.0f);
+    std::vector<float> h_Res(width * width, 0.0f);
+
+    cudaMalloc((void **)&M_d, size);
+    cudaMalloc((void **)&N_d, size);
+    cudaMalloc((void **)&P_d, size);
+
+    cudaMemcpy(M_d, h_Matrix_one.data(), size, cudaMemcpyHostToDevice);
+    cudaMemcpy(N_d, h_Matrix_two.data(), size, cudaMemcpyHostToDevice);
+
+    dim3 threadsPerBlock(2, 2);
+    dim3 numBlocks(1, 1);
+    //这样写不行 MatrixMulKernel<<<1,2,2>>>(M_d,N_d,P_d,width);
+    MatrixMulKernel<<<numBlocks,threadsPerBlock>>>(M_d,N_d,P_d,width);//这样可以，为什么
+
+    cudaMemcpy(h_Res.data(), P_d, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(M_d);
+    cudaFree(N_d);
+    cudaFree(P_d);
+    for(float elem:h_Res){
+        printf("%f\n",elem);
+    }
 }
--- vector_add.cu
+++ vector_add.cu
@@ -15,12 +15,15 @@ int main()
 {
     float *A_d, *B_d, *C_d;
     float size = 114514 * sizeof(float);
-    std::vector<float> A(114514,1);   
-    std::vector<float> B(114514,1);  
-    std::vector<float> C(114514,0);  
+
+    std::vector<float> A(114514,1.0f);   
+    std::vector<float> B(114514,1.0f);  
+    std::vector<float> C(114514,0.0f);  
+
     cudaMalloc((void **)&A_d, size);
     cudaMalloc((void **)&B_d, size);
     cudaMalloc((void **)&C_d, size);
+    
     cudaMemcpy(A_d, A.data(), size, cudaMemcpyHostToDevice);
     cudaMemcpy(B_d, B.data(), size, cudaMemcpyHostToDevice);
     // 第一个配置参数给出了网格中的块数，第二个指定了每个块中的线程数。在这个例子中，每个块中有256个线程。
@@ -29,5 +32,5 @@ int main()
     cudaFree(A_d);
     cudaFree(B_d);
     cudaFree(C_d);
-    printf("%f",C[1]);
+    printf("%f",C[123]);
 }
