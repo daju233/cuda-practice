efff5aecae1e2482b272a00a0ffc51e35b3a3ac0
daju233 <2212509690@qq.com>
2025-01-02 15:43:04 +0800

    向量加法

--- vector_add.cu
+++ vector_add.cu
@@ -1,5 +1,6 @@
 #include <cuda_runtime.h>
 #include<stdio.h>
+#include <vector>
 
 __global__ void vecAddKernel(float *A, float *B, float *C, int n)
 {
@@ -14,17 +15,17 @@ int main()
 {
     float *A_d, *B_d, *C_d;
     float size = 114514 * sizeof(float);
-    float A[114514]={1,1,1,1};
-    float B[114514]={1,1,1,1};
-    float C[114514]={0,0,0,0};
+    std::vector<float> A(114514,1);   
+    std::vector<float> B(114514,1);  
+    std::vector<float> C(114514,0);  
     cudaMalloc((void **)&A_d, size);
     cudaMalloc((void **)&B_d, size);
     cudaMalloc((void **)&C_d, size);
-    cudaMemcpy(A_d, A, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(B_d, B, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(A_d, A.data(), size, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B.data(), size, cudaMemcpyHostToDevice);
     // 第一个配置参数给出了网格中的块数，第二个指定了每个块中的线程数。在这个例子中，每个块中有256个线程。
     vecAddKernel<<<ceil(114514 / 256.0), 256.0>>>(A_d, B_d, C_d, size);
-    cudaMemcpy(C,C_d,size,cudaMemcpyDeviceToHost);
+    cudaMemcpy(C.data(),C_d,size,cudaMemcpyDeviceToHost);
     cudaFree(A_d);
     cudaFree(B_d);
     cudaFree(C_d);
